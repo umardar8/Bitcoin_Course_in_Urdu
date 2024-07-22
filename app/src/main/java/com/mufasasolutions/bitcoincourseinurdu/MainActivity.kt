d79fb31e81370ed3cf2deb89aec3204adf703dab
umardar8 <umerfarooqdar.official@gmail.com>
2024-07-23 03:35:59 +0500

    minor changes

--- app/src/main/java/com/mufasasolutions/bitcoincourseinurdu/MainActivity.kt
+++ app/src/main/java/com/mufasasolutions/bitcoincourseinurdu/MainActivity.kt
@@ -12,7 +12,6 @@ import com.mufasasolutions.bitcoincourseinurdu.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
